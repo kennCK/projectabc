d7547b883c3d699abadbb1fb985ebc0cd3401086
kennCK <kennettecanales@gmail.com>
2018-11-07 10:32:56 +0800

    update

--- src/modules/marketplace/Marketplace.vue
+++ src/modules/marketplace/Marketplace.vue
@@ -1,17 +1,46 @@
 <template>
-	<div class="template-holder">
-		<create></create>
-    <div class="template-list">
-      <item v-for="item, index in data" v-if="data !==null" :item="item" :key="item.id" :index="index"></item>
+  <div class="template-holder">
+    <div class="filter">
+      <div class="input-group">
+        <span class="input-group-addon bg-primary" style="width: 200px !important; background: #22b173 !important;">Marketplace</span>
+        <span class="input-group-addon">Search</span>
+        <input type="text" class="form-control" placeholder="Type something to find here...">
+
+        <span class="input-group-addon" style="margin-left: 20px;">Categories</span>
+        <select class="form-control">
+          <option>School</option>
+          <option>Office</option>
+        </select>
+      </div>
     </div>
-    <marketplace></marketplace>
-	</div>
+    <div class="template-list">
+      <product v-for="item, index in data" v-if="data !==null" :item="item" :key="item.id" :index="index"></product>
+    </div>
+  </div>
 </template>
 <style scoped>
 .template-holder{
   width: 100%;
   float: left;
 }
+.filter{
+  width: 100%;
+  float: left;
+  height: 50px;
+}
+
+.form-control{
+  height: 45px !important;
+}
+.input-group{
+  margin-bottom: 10px !important;
+}
+.input-group-addon{
+  width: 100px !important;
+  background: #22b173 !important;
+  color: #fff !important;
+}
+
 .template-list{
   width: 100%;
   float: left;
@@ -37,9 +66,7 @@ export default {
     }
   },
   components: {
-    'create': require('modules/editor/Create.vue'),
-    'item': require('modules/editor/Item.vue'),
-    'marketplace': require('modules/marketplace/Marketplace.vue')
+    'product': require('modules/marketplace/Product.vue')
   },
   methods: {
     redirect(parameter){
