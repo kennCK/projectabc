0dc3347f67e7addac0973843df493fe4ce7713ae
kennCK <kennettecanales@gmail.com>
2019-01-10 09:59:29 +0800

    update

--- api/database/migrations/2019_01_01_060650_update_plans_table_1.php
+++ api/database/migrations/2019_01_01_060650_update_plans_table_1.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdatePlansTable extends Migration
+class UpdatePlansTable1 extends Migration
 {
     /**
      * Run the migrations.
