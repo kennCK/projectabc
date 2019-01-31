83f39a7e959148e7724305f941708d5061368998
kennCK <kennettecanales@gmail.com>
2019-01-31 22:29:28 +0800

    update

--- api/app/Http/Controllers/CheckoutItemController.php
+++ api/app/Http/Controllers/CheckoutItemController.php
@@ -19,7 +19,7 @@ class CheckoutItemController extends APIController
         'payload' => $data['payload'],
         'payload_value' => $data['payload_value'],
         'price' => $data['price'],
-        'status' => 'added'
+        'status' => 'printing'
       );
     	$accountId = $data['account_id'];
     	$checkout = Checkout::where('account_id', '=', $accountId)->where('status', '=', 'added')->first();
