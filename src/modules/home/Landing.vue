f633a834f4606a74413731fb7e287f09dd4c47f6
jsalise <pjs11_midfield@yahoo.com>
2019-02-01 17:07:48 +0800

    update

--- src/modules/home/Landing.vue
+++ src/modules/home/Landing.vue
@@ -2,6 +2,7 @@
 	<div class="body">
 		<landing-banner></landing-banner>
     <landing-steps></landing-steps>
+    <landing-bizmarketplace></landing-bizmarketplace>
     <landing-mc></landing-mc>
     <landing-features></landing-features>
 	</div>
@@ -13,6 +14,7 @@ export default {
   components: {
     'landing-banner': require('modules/home/Landing/Banner.vue'),
     'landing-steps': require('modules/home/Landing/Steps.vue'),
+    'landing-bizmarketplace': require('modules/home/Landing/BizMarketplace.vue'),
     'landing-features': require('modules/home/Landing/Features.vue'),
     'landing-mc': require('modules/home/Landing/MessageAndComment.vue')
   },
