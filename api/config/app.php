8dfb958092f28c0982533df35c7f6d75f8c49ef8
kennCK <kennettecanales@gmail.com>
2019-03-25 23:10:45 +0800

    update

--- api/config/app.php
+++ api/config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         LaravelQRCode\Providers\QRCodeServiceProvider::class,
-        Increment\Comment\CommentServiceProvider::class,
+        // Increment\Comment\CommentServiceProvider::class,
     ],
 
     /*
