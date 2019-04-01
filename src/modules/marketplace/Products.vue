d72d24a44e9e1a58d7430ef8a3d00bdb41110cce
kennCK <kennettecanales@gmail.com>
2019-04-01 14:24:18 +0800

    update

--- src/modules/marketplace/Products.vue
+++ src/modules/marketplace/Products.vue
@@ -1,15 +1,24 @@
 <template>
   <div v-if="data !== null">
-    <div class="product-holder" v-for="item, index in data" @click="redirect('marketplace/product/' + item.code)">
+    <div class="product-holder" v-for="item, index in data">
       <div class="product-image">
-       <img :src="config.BACKEND_URL + item.featured.url" v-if="item.featured !== null">
-       <i class="fa fa-image" v-else></i>
+        <img :src="config.BACKEND_URL + item.featured.url" v-if="item.featured !== null">
+        <i class="fa fa-image" v-else></i>
+        <div class="product-wishlist bg-primary" v-if="item.wishlist_flag === false && item.checkout_flag === false">
+          <label>
+            <i class="fa fa-heart"  @click="addToWishlist(item.id)" v-if="item.wishlist_flag === false && item.checkout_flag === false"></i>  
+          </label>
+          <label @click="redirect('marketplace/product/' + item.code)" style="border-left: solid 1px #ddd;">View</label>
+        </div>
+        <div class="product-wishlist bg-primary" v-else>
+          <label @click="redirect('marketplace/product/' + item.code)" style="width: 100%;">View</label>
+        </div>
       </div>
       <div class="product-details">
         <div class="product-title">
           <label style="padding-top: 5px;"><b>{{item.title}}</b></label>
           <label>{{item.description}}</label>
-        </div>
+        </div>  
         <div class="product-price">
           <label v-if="item.price !== null">
             <label v-if="item.price.length === 1">PHP {{item.price[0].price}}</label>
@@ -32,14 +41,15 @@
     margin-top: 25px;
   }
   .product-holder:hover{
-    cursor: pointer;
     border: solid 1px #ffaa81;
     background: #ffaa81;
     color: #fff;
   }
+
   .product-image{
     width: 100%;
     float: left;
+    position: relative;
     height: 250px;
     text-align: center;
   }
@@ -82,6 +92,37 @@
     padding-right: 5px;
   }
 
+  .product-wishlist{
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    z-index: 10;
+    display: none;
+  }
+
+  .product-wishlist:hover, .product-wishlist i:hover, .product-wishlist label:hover{
+    cursor: pointer;
+  }
+
+  .product-wishlist label{
+    line-height: 50px;
+    float: left;
+    width: 50%;
+    text-align: center;
+  }
+  
+  .product-wishlist i{
+    font-size: 32px;
+    line-height: 50px;
+  }
+
+  .product-holder:hover .product-wishlist{
+    display: block;
+  }
+
 </style>
 <script>
 import ROUTER from '../../router'
@@ -102,6 +143,18 @@ export default {
   methods: {
     redirect(parameter){
       ROUTER.push(parameter)
+    },
+    addToWishlist(id){
+      let parameter = {
+        payload: 'product',
+        payload_value: id,
+        account_id: this.user.userID
+      }
+      $('#loading').css({display: 'block'})
+      this.APIRequest('wishlists/create', parameter).then(response => {
+        $('#loading').css({display: 'none'})
+        this.$parent.retrieve()
+      })
     }
   }
 }
