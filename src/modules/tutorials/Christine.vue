daebc5c9b42f7a266a8bd94f917546f0eb5f1b0e
nekewl <christinenicoledelossantos@gmail.com>
2019-07-02 15:55:06 +0800

    AudioCall

---
+++ src/modules/tutorials/Christine.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    christine here...
+  <button class="btn btn-primary" @click="showAudio">Call</button>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+import AUTH from 'src/services/auth'
+export default {
+  methods: {
+    showAudio(){
+      AUTH.triggerAudioCall()
+    }
+  }
+}
+</script>
