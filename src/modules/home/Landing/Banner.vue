a091a07ff0e8f09f88834e637aae43162a132678
hannype <penarubiahanny@gmail.com>
2019-07-03 16:29:06 +0800

    landing page

--- src/modules/home/Landing/Banner.vue
+++ src/modules/home/Landing/Banner.vue
@@ -12,23 +12,23 @@
       </span>
     </div> 
     <div class="division">
-      <div class="device">
-      </div>
+      <div class="phone-wrapper">
+    <label class="speaker"></label>
+    <div class="phone-body">
+      <HtmlContent :content="content"></HtmlContent>
+    </div>
+    <label class="home-swipe">
+      <font-awesome-icon icon="caret-left" class="icon"></font-awesome-icon>
+      <font-awesome-icon icon="circle" class="icon"></font-awesome-icon>
+      <font-awesome-icon icon="square" class="icon"></font-awesome-icon>
+    </label>
+  </div>
    </div> 
  </div>
 </template>
 </span>
 <style scoped>
-.device{
-  border: 1px solid black;
-  width: 350px;
-  float: left;
-  height: 400px;
-  overflow-y: auto;
-  margin-top: 40px;
-  margin-bottom: 20px;
-  font-size: 90px;
-}
+
 .division{
   border:1px solid black;
   width: 50%;
@@ -93,6 +93,58 @@
     margin-right: 0%;
     padding: 0;
   }
+.phone-wrapper{
+  width: 100%;
+  height: 100%;
+  float: left;
+  border-radius: 20px;
+  background: black;
+  position: relative;
+}
+
+.phone-body{
+  width: 96%;
+  height: 97%;
+  margin: 2%;
+  background:white;
+  border-radius: 20px;
+  padding-top: 25px;
+}
+.phone-body{
+  width: 96%;
+  height: 97%;
+  margin: 2%;
+  background:white;
+  border-radius: 20px;
+  padding-top: 25px;
+}
+.speaker{
+  background: black;
+  height: 25px;
+  width: 10%;
+  margin-left: 45%;
+  position: absolute;
+  border-bottom-left-radius: 25px;
+  border-bottom-right-radius: 25px;
+}
+.home-swipe{
+  height: 35px;
+  width: 96%;
+  margin-left: 2%;
+  margin-right: 2%;
+  bottom: 0px;
+  background: white;
+  border-top: solid 1px $hover;
+  position: absolute;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  text-align: center;
+}
+.home-swipe .icon{
+  font-size: 16px;
+  width: 20%;
+  margin-top: 10px;
+}
 }
 </style>
 <script>
