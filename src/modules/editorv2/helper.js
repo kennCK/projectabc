f6a7237b54dac5cc0ce92443bf2caeaa5512fbd3
kennCK <kennettecanales@gmail.com>
2019-07-23 10:37:50 +0800

    Fixed zoom on objects

--- src/modules/editorv2/helper.js
+++ src/modules/editorv2/helper.js
@@ -7,13 +7,22 @@ export default {
     // get border
     // get the ratio of pageSize
     // return the style
-    let tempStyle = style
+    let tempStyle = {}
+    let modifiedArray = ['height', 'width', 'top', 'left', 'borderRadius']
     zoom = parseInt(zoom)
-    tempStyle.height = parseInt(parseInt(style.height) * (zoom / 100)) + setting.units
-    tempStyle.width = parseInt(parseInt(style.width) * (zoom / 100)) + setting.units
-    tempStyle.top = parseInt(parseInt(style.top) * (zoom / 100)) + setting.units
-    tempStyle.left = parseInt(parseInt(style.left) * (zoom / 100)) + setting.units
-    tempStyle.borderRadius = parseInt(parseInt(style.borderRadius) * (zoom / 100)) + setting.units
+    tempStyle['height'] = parseInt(parseInt(style.height) * (zoom / 100)) + setting.units
+    tempStyle['width'] = parseInt(parseInt(style.width) * (zoom / 100)) + setting.units
+    tempStyle['top'] = parseInt(parseInt(style.top) * (zoom / 100)) + setting.units
+    tempStyle['left'] = parseInt(parseInt(style.left) * (zoom / 100)) + setting.units
+    tempStyle['borderRadius'] = parseInt(parseInt(style.borderRadius) * (zoom / 100)) + setting.units
+    let objectAttributes = Object.keys(style)
+    for (var i = 0; i < objectAttributes.length; i++) {
+      if(modifiedArray.indexOf(objectAttributes[i]) > -1){
+        //
+      }else{
+        tempStyle[objectAttributes[i]] = style[objectAttributes[i]]
+      }
+    }
     return tempStyle
   }
 }
