29d925af631ea8fdc84129616faa8e75999587e3
kennCK <kennettecanales@gmail.com>
2019-07-23 09:56:43 +0800

    Zoom

--- src/modules/editorv2/helper.js
+++ src/modules/editorv2/helper.js
@@ -1,12 +1,19 @@
 export default {
-  style (style, zoom, pageSize) {
+  style (style, zoom, setting) {
     // get height
     // get width
-    // get position
+    // get position top / left
     // get border radius
     // get border
     // get the ratio of pageSize
     // return the style
-    return style
+    let tempStyle = style
+    zoom = parseInt(zoom)
+    tempStyle.height = parseInt(parseInt(style.height) * (zoom / 100)) + setting.units
+    tempStyle.width = parseInt(parseInt(style.width) * (zoom / 100)) + setting.units
+    tempStyle.top = parseInt(parseInt(style.top) * (zoom / 100)) + setting.units
+    tempStyle.left = parseInt(parseInt(style.left) * (zoom / 100)) + setting.units
+    tempStyle.borderRadius = parseInt(parseInt(style.borderRadius) * (zoom / 100)) + setting.units
+    return tempStyle
   }
 }
